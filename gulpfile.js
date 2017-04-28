658737a67dc3a53d15a23d62b6b32f9eddfd28b3
Asami Doi <doiasami1219@gmail.com>
2017-04-28 13:11:37 +0900

    modify: gulp setting

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,58 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var ejs = require("gulp-ejs");
+var autoprefixer = require("gulp-autoprefixer");
+var uglify = require("gulp-uglify");
+var browser = require("browser-sync");
+var plumber = require("gulp-plumber");
+var rename = require("gulp-rename");
+var webpack = require('gulp-webpack');
 
-gulp.task('sass', function(){
-  gulp.src('./sass/**/*.scss')
-    .pipe(sass({outputStyle: 'expanded'}))
-    .pipe(gulp.dest('./css/'));
+gulp.task("server", function() {
+    browser({
+        server: {
+            baseDir: "./"
+        }
+    });
 });
 
-// gulp.task('sass-watch', ['sass'], function()
-//   var watcher = gulp.watch('./sass/**/*.scss', ['sass']);
-//   watcher.on('change', function(event) {
-//   });
-// });
-//
-// gulp.task('default', ['sass-watch']);
+gulp.task("sass", function() {
+    gulp.src(["sass/**/*scss", "!sass/**/_*.scss"])
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest("./css"))
+        .pipe(browser.reload({stream:true}))
+});
+
+gulp.task("js", function() {
+    gulp.src(["js/bundle.js"])
+        .pipe(plumber())
+        .pipe(uglify())
+        .pipe(gulp.dest("./js/min"))
+        .pipe(browser.reload({stream:true}))
+});
+
+gulp.task("ejs", function() {
+    gulp.src(["ejs/**/*.ejs","!ejs/**/_*.ejs"])
+        .pipe(plumber())
+        .pipe(ejs())
+        .pipe(rename({extname: '.html'}))
+        .pipe(gulp.dest("./"))
+        .pipe(browser.reload({stream:true}))
+});
+
+gulp.task("webpack", function() {
+    gulp.src(["js/**/*.js","!js/bundle.js","!js/min/**/*.js"])
+        .pipe(plumber())
+        .pipe(webpack(require('./webpack.config.js')))
+        .pipe(gulp.dest("./"))
+        .pipe(browser.reload({stream:true}))
+});
+
+gulp.task("default", ['server'], function() {
+    gulp.watch("sass/**/*.scss",["sass"]);
+    gulp.watch("js/**/*.js",["webpack"]);
+    gulp.watch("js/**/*.js",["js"]);
+    gulp.watch(["ejs/**/*.ejs", "ejs/common/**/_*.ejs"],["ejs"]);
+});
